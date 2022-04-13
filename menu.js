42eac05a14c06d238ae2a0ee118a65986071a6d4
nyaruref <nyaruref@gmail.com>
2022-04-13 03:06:08 -0400

    Create menu.js

---
+++ menu.js
@@ -0,0 +1,42 @@
+const smallScreen = window.innerWidth < 800 || window.innerHeight < 800;
+
+if(!smallScreen) {
+	document.body.classList.add('big');
+	
+	const headers = new Headers({'Content-Type': 'text/xml'});
+	const myRequest = new Request('../menu.html');
+	
+	fetch(myRequest, {headers})
+	.then((response) => {
+		if(!response.ok) {
+			throw new Error(`HTTP error! Status: ${ response.status }`);
+		}
+		return response.text();
+	})
+	.then((response) => {
+		const menu = document.createElement('DIV');
+		menu.innerHTML = response;
+		document.body.appendChild(menu);
+		animateMenu();
+	});
+}
+else {
+	document.body.classList.add('small');
+}
+
+function animateMenu() {
+	var dropdown = document.getElementsByClassName("dropdown-button");
+	var i;
+	for (i = 0; i < dropdown.length; i++) {
+		dropdown[i].addEventListener("click", function() {
+			this.classList.toggle("dropdown-active");
+			var dropdownContent = this.nextElementSibling;
+			if (dropdownContent.style.display === "block") {
+				dropdownContent.style.display = "none";
+			}
+			else {
+				dropdownContent.style.display = "block";
+			}
+		});
+	}
+}
